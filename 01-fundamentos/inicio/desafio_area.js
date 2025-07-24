74bdf0576720d1b123e8e73ae0514c49d3ca3f90
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2025-07-24 20:57:38 +0200

    Desafio Desafio Área da Circunferência

---
+++ 01-fundamentos/inicio/desafio_area.js
@@ -0,0 +1,10 @@
+// area = PI * raio * raio
+
+const PI = 3.14;
+let raio = 10;
+let areaCirc = PI * raio * raio; //Math.PI const do PI em JS
+
+console.log("O resultado da área é: " + areaCirc + "m2.");
+
+areaCirc = Math.PI * raio * raio;
+console.log("O resultado da área é: " + areaCirc + "m2.");
