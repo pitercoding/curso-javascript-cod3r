d332c5291b7d437d4dae24377dcc4c0b93b71f12
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2025-07-24 20:47:59 +0200

    Aula 25. Trabalhando com Dados #03

---
+++ 01-fundamentos/inicio/dados_3.js
@@ -0,0 +1,14 @@
+let idade = 31;
+console.log(typeof 31);
+console.log(typeof idade);
+
+console.log(typeof -45631);
+
+let salario = 4567.88;
+console.log(typeof salario);
+
+let estaChovendo = true;
+console.log(typeof estaChovendo);
+
+console.log(typeof "Teste");
+console.log(typeof 'Teste');
