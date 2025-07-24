bfb548864227cc3fc1dca470020a571e934a519c
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2025-07-24 15:14:18 +0200

    Aula 19. Comentários de Código

---
+++ 01-fundamentos/inicio/comentario.js
@@ -0,0 +1,11 @@
+
+//console.log é usado para exibir algo na tela do pc
+console.log("Passo #01");
+console.log("Passo #02"); //mais um exemplo de comentário
+// console.log("Passo #03");
+
+/**
+ * Comentário 
+ * de multiplas 
+ * linhas
+ */
