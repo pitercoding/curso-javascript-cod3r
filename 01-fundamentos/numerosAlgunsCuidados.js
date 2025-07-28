42958d36b49682d2d4c616cff2ae93e924e07fd7
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2025-07-28 20:35:19 +0200

    Aula 41. Number: Alguns Cuidados

---
+++ 01-fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,7 @@
+console.log(7/0); //Infinity. Não gera erro.
+console.log("10"/2); //Funciona em JS
+console.log("Show"*2); //Não funciona e retorna NaN
+console.log(0.1 + 0.7); //Não é preciso
+//console.log(10.toString());
+console.log((10).toFixed()); //Funciona
+console.log((10.3456).toFixed(2)); //Funciona
