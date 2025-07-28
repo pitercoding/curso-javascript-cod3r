103de2788491bccced879768fa5fdcbfed687d03
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2025-07-28 17:01:31 +0200

    Aula 37. Comentários de Código

---
+++ 01-fundamentos/fundamentos/comentario.js
@@ -0,0 +1,15 @@
+//Comentário de uma linha
+console.log('Linha 1');
+
+/*
+Comentários de
+múltiplas linhas
+*/
+console.log('Linha 2');
+
+/*
+* Comentários de
+* múltiplas linhas
+*/
+console.log('Linha 3');
+
