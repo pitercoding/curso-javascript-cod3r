73a2000882a6a5d332dc194627a13e90a0032371
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2025-07-29 14:27:47 +0200

    Aula 44. Usando Template Strings

---
+++ 01-fundamentos/templateString.js
@@ -0,0 +1,17 @@
+//Concatenando com +
+const nome = 'Rebeca';
+const concatencao = "Olá " + nome + "!";
+console.log(concatencao);
+
+//Concatenando com Template String
+const template = `Olá, ${nome}!`;
+console.log(template);
+
+//Expressões
+let soma = 1 + 1;
+console.log(`1 + 1 = ${soma}`);
+
+//Textos
+const up = texto => texto.toUpperCase();
+console.log(`Ei... ${up("cuidado")}!`);
+
