34984f2df16d141b77ef77bd45b6e40f65487298
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2025-08-01 16:11:40 +0200

    Aula 68. Operadores: Aritméticos

---
+++ 01-fundamentos/aritmeticos.js
@@ -0,0 +1,14 @@
+const [a, b, c, d] = [3, 5, 1, 15]
+
+const soma = a + b + c + d
+const subtracao = d - b
+const multiplicacao = a * b
+const divisao = d / a
+const modulo = a % 2
+
+console.log(soma) //24
+console.log(subtracao) //10
+console.log(multiplicacao) //15
+console.log(divisao) //5
+console.log(modulo) //1
+
