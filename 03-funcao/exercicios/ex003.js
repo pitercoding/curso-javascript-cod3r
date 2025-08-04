0cd87b2fbe9e745d8f3292b7f16f4ebb06740379
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2025-08-04 21:05:44 +0200

    exercício 003

---
+++ 03-funcao/exercicios/ex003.js
@@ -0,0 +1,8 @@
+// Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.
+
+function calcularPotencia (base, expoente) {
+    resultado = base ** expoente
+    return resultado
+}
+
+console.log(calcularPotencia(2,3))
