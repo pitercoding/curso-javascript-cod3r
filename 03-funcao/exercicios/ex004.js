7d12cd6b1b0414cdd938c969716761828ec48137
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2025-08-04 21:32:32 +0200

    exercício 004

---
+++ 03-funcao/exercicios/ex004.js
@@ -0,0 +1,8 @@
+// Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá imprimir o resultado e o resto da divisão destes dois valores.
+
+function dividindo(dividendo, divisor) {
+    console.log("Resultado: " + Math.floor(dividendo / divisor))
+    console.log(`Resto:  ${dividendo % divisor}`)
+}
+
+dividindo(11,4)
