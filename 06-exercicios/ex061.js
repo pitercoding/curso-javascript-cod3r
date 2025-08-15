56a3ce8bd19926938ff543ce03c7cfc3a957a17b
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2025-08-15 15:38:51 +0200

    exercício 061

---
+++ 06-exercicios/ex061.js
@@ -0,0 +1,8 @@
+function areaDoTriangulo(base, altura) {
+    const area = (base * altura) / 2
+
+    return area.toFixed(2)
+}
+
+console.log(areaDoTriangulo(10, 15)) //75
+console.log(areaDoTriangulo(7, 9)) // 31,50
