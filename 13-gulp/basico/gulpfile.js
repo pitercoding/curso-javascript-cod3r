1acc1cab266dd1b2b97b8e77b9c49249777b601c
pitercoding <piterg.bio@gmail.com>
2025-11-12 08:54:42 +0100

    feat(aula): 319. Fundamentos de Gulp #02

--- 13-gulp/basico/gulpfile.js
+++ 13-gulp/basico/gulpfile.js
@@ -1,9 +1,36 @@
 const gulp = require('gulp')
-const series = gulp.series
+const { series, parallel } = require('gulp')
 
-function copiar(cb) {
-    console.log('Tarefa de copiar!')
-    return cb
+const antes1 = cb => {
+    console.log('Tarefa Antes 1!')
+    return cb()
 }
 
-module.exports.default = series(copiar)
+const antes2 = cb => {
+    console.log('Tarefa Antes 2!')
+    return cb()
+}
+
+function copiar(cb) {
+    // gulp.src(['pastaA/arquivo1.txt', 'pastaA/arquivo2.txt'])
+
+    gulp.src('pastaA/**/*.txt')
+        // .pipe(imagePelaMetade())
+        // .pipe(imageEmPretoEBranco())
+        // .pipe(transformacaoA())
+        // .pipe(transformacaoB())
+        // .pipe(transformacaoC())
+        .pipe(gulp.dest('pastaB'))
+    return cb()
+}
+
+const fim = cb => {
+    console.log('Tarefa Fim!')
+    return cb()
+}
+
+module.exports.default = series(
+    parallel(antes1, antes2),
+    copiar,
+    fim,
+)
