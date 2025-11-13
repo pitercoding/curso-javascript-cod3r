4c6fd4bb8e0c5daac51e5b0e6cbdfd98e3f3a30e
pitercoding <piterg.bio@gmail.com>
2025-11-13 09:29:03 +0100

    feat(aula): 339. Carregando Arquivo .CSS

--- 14-webpack/webpack.config.js
+++ 14-webpack/webpack.config.js
@@ -1,10 +1,21 @@
-const webpack = require('webpack')
+const webpack = require("webpack");
 
 module.exports = {
-    mode: 'development',
-    entry: './src/principal.js',
-    output: {
-        filename: 'principal.js',
-        path: __dirname + '/public'
-    }
-}
+  mode: "development",
+  entry: "./src/principal.js",
+  output: {
+    filename: "principal.js",
+    path: __dirname + "/public",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader", // Adiciona CSS a DOM injetando a tag <style></style>
+          "css-loader", // Interpreta imports, url()...
+        ],
+      },
+    ],
+  },
+};
