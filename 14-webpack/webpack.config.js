edfd3d85e2685b4055c5d4bb65ec19b3e97966ca
pitercoding <piterg.bio@gmail.com>
2025-11-13 09:49:10 +0100

    feat(aula): 342. Carregando Arquivo .SCSS (SASS)

--- 14-webpack/webpack.config.js
+++ 14-webpack/webpack.config.js
@@ -16,11 +16,12 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /\.s?[ac]ss$/,
         use: [
           MiniCssExtractPlugin.loader,
           //"style-loader", // Adiciona CSS a DOM injetando a tag <style></style>
           "css-loader", // Interpreta imports, url()...
+          "sass-loader",
         ],
       },
     ],
