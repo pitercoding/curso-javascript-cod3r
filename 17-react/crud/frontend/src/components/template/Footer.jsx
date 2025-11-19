2c1e5eb73235524e7bf6e555d275919841ac8056
pitercoding <piterg.bio@gmail.com>
2025-11-19 20:09:30 +0100

    feat(aula): 424. Componente Rodapé

--- 17-react/crud/frontend/src/components/template/Footer.jsx
+++ 17-react/crud/frontend/src/components/template/Footer.jsx
@@ -3,5 +3,7 @@ import React from 'react';
 
 export default props =>
     <footer className="footer">
-        Rodapé
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por <strong> Pit<span className='text-danger'>e</span>r</strong>
+        </span>
     </footer>
