e09f449f0a9a93bdd22bbe74be31b1ff6b3a87b6
pitercoding <piterg.bio@gmail.com>
2025-11-19 10:56:06 +0100

    feat(aula): 408. Componentes: Pai & Filho #02

--- 17-react/exercicios/src/index.js
+++ 17-react/exercicios/src/index.js
@@ -4,11 +4,16 @@ import ReactDOM from "react-dom/client";
 // import { BoaTarde, BoaNoite } from "./componentes/Multiplos";
 // import Saudacao from "./componentes/Saudacao";
 import Pai from "./componentes/Pai";
+import Filho from "./componentes/Filho";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <>
-    <Pai nome={"Paulo"} sobrenome={"Silva"} />
+    <Pai nome={"Paulo"} sobrenome={"Silva"} >
+        <Filho nome={"Pedro"} sobrenome={"Silva" } />
+        <Filho nome={"Ana"} sobrenome={"Silva" } />
+        <Filho nome={"Gui"} sobrenome={"Silva" } />
+    </Pai>
   </>
 );
