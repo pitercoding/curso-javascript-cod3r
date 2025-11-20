4d9eb153b5e0d1a911bf45fca5b6c49163c5a25f
pitercoding <piterg.bio@gmail.com>
2025-11-20 20:29:12 +0100

    feat(aula): 441. CSS Modularizado

---
+++ 18-nextjs/fundamentos/src/app/estiloso/page.jsx
@@ -0,0 +1,9 @@
+import styles from './Estiloso.module.css';
+
+export default function Estiloso() {
+    return (
+        <div className={styles.roxo}>
+            <h1>Estilo Usando CSS Módulos</h1>
+        </div>
+    )
+}
