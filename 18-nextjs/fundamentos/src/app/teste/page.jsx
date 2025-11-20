366fed5cbaf451c1098d9bbe1bbc9ebf8f55cd81
pitercoding <piterg.bio@gmail.com>
2025-11-20 12:31:30 +0100

    feat(aula): 437. Primeiro Componente & Rotas

---
+++ 18-nextjs/fundamentos/src/app/teste/page.jsx
@@ -0,0 +1,8 @@
+export default function Teste() {
+    return (
+        <div>
+            <h1>Página de Teste!</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit libero sapiente quia pariatur laborum asperiores aspernatur omnis reprehenderit rem incidunt, molestiae culpa blanditiis id fugiat provident, in dolorum illo. Veniam.</p>
+        </div>
+    )
+}
