f4252522edebc34a069f477e7b660353ae62e11e
pitercoding <piterg.bio@gmail.com>
2025-11-22 10:44:07 +0100

    feat(aula): 454. Modelos de Pré-Renderização

--- 18-nextjs/fundamentos/src/app/page.tsx
+++ 18-nextjs/fundamentos/src/app/page.tsx
@@ -14,8 +14,8 @@ export default function Inicio() {
       <Navegador texto="JSX" destino="/jsx" cor="crimson"/>
       <Navegador texto="Navegação #01" destino="/navegacao" cor="green"/>
       <Navegador texto="Navegação #02" destino="/cliente/sp-2/321" cor="blue"/>
-      <Navegador texto="Componente Com Estado" destino="/estado" cor="pink"/>
-      <Navegador texto="Integração Com API #1" destino="/integracao_1" cor="yellow"/>
+      <Navegador texto="Componente Com Estado" destino="/estado" cor="#A45B71"/>
+      <Navegador texto="Integração Com API #1" destino="/integracao_1" cor="#42a9a9"/>
     </div>
   )
 }
--- 18-nextjs/fundamentos/src/components/Navegador.jsx
+++ 18-nextjs/fundamentos/src/components/Navegador.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import styles from "./Navegador.module.css";
 
 export default function Navegador(props) {
-  return <Link href={props.destino}>
+  return <Link href={props.destino} passHref>
             <div className={styles.navegador} style={{
                 backgroundColor: props.cor ?? 'dodgerblue'
             }}>
