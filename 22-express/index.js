5f56040a3b959dc206c1856f112f97f4dd5c91a2
pitercoding <piterg.bio@gmail.com>
2025-12-03 12:28:40 +0100

    feat(aula): 554. Tipos de Resposta

--- 22-express/index.js
+++ 22-express/index.js
@@ -1,9 +1,27 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.all('/opa', (req, res) => {
-    res.send('<h1>Estou bem!</h1>')
-})
+app.get("/opa", (req, res) => {
+  res.json({
+    data: [
+      { id: 7, name: "Ana", position: 1 },
+      { id: 34, name: "Bia", position: 2 },
+      { id: 73, name: "Carlos", position: 3 },
+    ],
+    count: 30,
+    skip: 0,
+    limit: 3,
+    status: 200
+  });
+
+  // res.json({
+  //     nome: "Ipad 32Gb",
+  //     price: 1899,
+  //     discount: 0.12
+  // })
+
+  // res.send('<h1>Estou bem!</h1><br><br><h2>Tipo é HTML</h2>')
+});
 app.listen(3000, () => {
-    console.log("Backend Ativo...")
-})
+  console.log("Backend Ativo...");
+});
