00ea38b53cedaa6b12fbb38b87229a8eac0a18c2
pitercoding <piterg.bio@gmail.com>
2025-12-03 14:02:37 +0100

    feat(aula): 559. Comunicação Entre Módulos #01 o Entre Módulos #01

--- 22-express/index.js
+++ 22-express/index.js
@@ -3,6 +3,10 @@ const app = express();
 const bodyParser = require('body-parser')
 
 const saudacao = require('./saudacaoMid')
+const usuarioApi = require('./api/usuario')
+
+app.post('/usuario', usuarioApi.salvar)
+app.get('/usuario', usuarioApi.obter)
 
 app.use(bodyParser.text())
 app.use(bodyParser.json())
