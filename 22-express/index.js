b9b0fa8af3f16408c79f342a3fdb9320f8fe2ad7
pitercoding <piterg.bio@gmail.com>
2025-12-03 14:06:09 +0100

    feat(aula): 560. Comunicação Entre Módulos #02

--- 22-express/index.js
+++ 22-express/index.js
@@ -1,9 +1,12 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 const bodyParser = require('body-parser')
 
 const saudacao = require('./saudacaoMid')
 const usuarioApi = require('./api/usuario')
+const produtoApi = require('./api/produto')
+produtoApi(app, 'com param!')
+
 
 app.post('/usuario', usuarioApi.salvar)
 app.get('/usuario', usuarioApi.obter)
@@ -11,12 +14,13 @@ app.get('/usuario', usuarioApi.obter)
 app.use(bodyParser.text())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(saudacao('Piter'))
+
+app.use(saudacao('Guilherme'))
 
 app.use((req, res, next) => {
-  console.log("Antes...");
-  next()
-});
+    console.log('Antes...')
+    next()
+})
 
 app.get('/clientes/relatorio', (req, res) => {
     res.send(`Cliente relatório: completo = ${req.query.completo} ano = ${req.query.ano}`)
@@ -38,36 +42,35 @@ app.get('/clientes/:id', (req, res) => {
     res.send(`Cliente ${req.params.id} selecionado!`)
 })
 
-app.get("/opa", (req, res, next) => {
-  console.log("Durante...");
-  res.json({
-    data: [
-      { id: 7, name: "Ana", position: 1 },
-      { id: 34, name: "Bia", position: 2 },
-      { id: 73, name: "Carlos", position: 3 },
-    ],
-    count: 30,
-    skip: 0,
-    limit: 3,
-    status: 200,
-  });
+app.get('/opa', (req, res, next) => {
+    console.log('Durante...')
+    res.json({
+        data: [
+            {id: 7, name: 'Ana', position: 1 },
+            {id: 34, name: 'Bia', position: 2 },
+            {id: 73, name: 'Carlos', position: 3 }
+        ],
+        count: 30,
+        skip: 0,
+        limit: 3,
+        status: 200
+    })
 
-  next()
+    next()
+    
+    // res.json({
+    //     name: 'iPad 32Gb',
+    //     price: 1899.00,
+    //     discount: 0.12
+    // })
 
-  // res.json({
-  //     nome: "Ipad 32Gb",
-  //     price: 1899,
-  //     discount: 0.12
-  // })
-
-  // res.send('<h1>Estou bem!</h1><br><br><h2>Tipo é HTML</h2>')
-});
+    // res.send('<h1>Estou bem!</h1><br><br><h2>Tipo é HTML!</h2>')
+})
 
 app.use((req, res) => {
-  console.log("Depois...");
-});
-
+    console.log('Depois...')
+})
 
 app.listen(3000, () => {
-  console.log("Backend Ativo...");
-});
+    console.log('Backend executando...')
+})
