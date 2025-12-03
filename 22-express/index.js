bc7d79768526d4c8aec7ec59db9633a467ef1f68
pitercoding <piterg.bio@gmail.com>
2025-12-03 09:40:24 +0100

    feat(aula): 553. Tratando a Primeira Requisição

--- 22-express/index.js
+++ 22-express/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+app.all('/opa', (req, res) => {
+    res.send('<h1>Estou bem!</h1>')
+})
 app.listen(3000, () => {
     console.log("Backend Ativo...")
 })
